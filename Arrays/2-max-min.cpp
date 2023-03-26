3627f48725b60ebe9898b2770ca171a157579629
deeepanshu17 <86081382+deeepanshu17@users.noreply.github.com>
2023-03-26 16:06:53 +0530

    Add files via upload

---
+++ Arrays/2-max-min.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int sumOfMaxMin(int arr[], int n){
+	 int max=arr[0]; int sum=0;
+    for(int i=1;i<n;i++){
+        if(arr[i]>max)
+            max=arr[i];
+        }
+    int min=arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]<min)
+            min=arr[i];
+        }  
+     sum=min+max;
+     return sum;     
+        
+}
+
+
+
+
+int main(){
+      int n;
+      cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    cout<<sumOfMaxMin(arr,n);
+
+}
